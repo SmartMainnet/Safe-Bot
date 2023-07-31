f51932d06b000e9339a5f5cc5239e0c271bd7507
SmartMainnet <smartmainnet@gmail.com>
2023-07-31 18:39:59 +0700

    Update

--- index.js
+++ index.js
@@ -149,11 +149,11 @@ const audit = async (chatId, msgId, address) => {
             reply_markup: JSON.stringify({
               inline_keyboard: [
                 [
-                  { text: 'Audit', url: `https://gopluslabs.io/token-security/56/${address}` },
+                  { text: 'Audit', url: `https://gopluslabs.io/token-security/${isContractAddressBNB ? 56 : 1}/${address}` },
                   { text: 'Contract', url: `https://bscscan.com/token/${address}#balances` }
                 ],
                 [
-                  { text: 'Buy', url: `https://pancakeswap.finance/swap?outputCurrency=${address}&inputCurrency=BNB` },
+                  { text: 'Buy', url: `https://app.1inch.io/#/${isContractAddressBNB ? 56 : 1}/simple/swap/${isContractAddressBNB ? 'BNB' : 'ETH'}/${address}` },
                   { text: 'Chart', url: `https://poocoin.app/tokens/${address}` }
                 ],
                 [{ text: buttonText, url: url }]

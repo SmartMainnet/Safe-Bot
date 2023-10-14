0ca3147269f2335b903793fc968a9b5dc088711a
SmartMainnet <smartmainnet@gmail.com>
2023-10-14 14:46:17 +0700

    Update rateLimit

--- src/middlewares/core/limit.middleware.js
+++ src/middlewares/core/limit.middleware.js
@@ -1,8 +1,8 @@
 import rateLimit from 'telegraf-ratelimit'
 
 const limitConfig = {
-  window: 3000,
-  limit: 1,
+  window: 2000,
+  limit: 2,
   onLimitExceeded: ctx => ctx.reply(ctx.i18n.t('limit'))
 }
 
